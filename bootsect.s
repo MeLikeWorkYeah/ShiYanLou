e98a73aea1f515b34e48012b440583a937a4422b
MeLikeWorkYeah <bamlee@126.com>
2020-02-16 14:53:02 +0000

    update for HITOS-Lab2-6.5

--- bootsect.s
+++ bootsect.s
@@ -1,3 +1,5 @@
+SETUPLEN=2
+SETUPSEG=0x07e0!seg:0x07c0+512KB=seg:0x07e0
 entry _start
 _start:
 	mov ah,#0x03
@@ -12,8 +14,19 @@ _start:
 	mov bp,#msg1
 	mov ax,#0x1301
 	int 0x10
-inf_loop:
-	jmp inf_loop
+load_setup:
+	mov dx,#0x0000!drive 0,head 0
+	mov cx,#0x0002!sector 2,track 0
+	mov bx,#0x0200!read sectors to address es:bx
+	mov ax,#0x0200+SETUPLEN!ah:read func;al:num of sectors
+	int 0x13
+	jnc ok_load_setup
+	mov dx,#0x0000
+	mov ax,#0x0000!reset disk
+	int 13
+	jmp load_setup
+ok_load_setup:
+	jmpi 0,SETUPSEG
 msg1:
 	.byte 13,10
 	.ascii "Toy Kernel Loading"
